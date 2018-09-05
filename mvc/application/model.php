2a221c79c44eab9ecee6405cbec8dbb6aef825dc
Luisferh17 <luisferh17@gmail.com>
2018-09-04 21:12:24 -0500

    mvc

---
+++ mvc/application/model.php
@@ -0,0 +1,14 @@
+<?php
+
+class Model {
+ public function getInfo() {
+ return array(
+ 'firstName' 	=> 'Jeremias',
+ 'lastName' 	=> 'Sprindfield',
+ 'eMail' 		=> 'jeremias@gmail.com',
+ 'phoneNumber' 	=> 3120909900
+ );
+ }
+}
+
+?>
---
+++ mvc/views/home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ 	<meta charset="UTF-8">
+	 <title>CRUD MVC</title>
+</head>
+<body>
+ 	<h1> CRUD MVC </h1>
+ 	<hr> 
+ <strong> FirstName: </strong><?php echo $data['firstName']; ?>
+ <br>
+ <strong> LastName: </strong><?php echo $data['lastName']; ?>
+ <br>
+ <strong> Email: </strong><?php echo $data['eMail']; ?>
+ <br>
+ <strong> PhoneNumber: </strong><?php echo $data['phoneNumber']; ?>
+ 
+
+</body>
+</html>
