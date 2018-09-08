a4f310ec3df5c4ebbb291d552033396a39d6e6ab
Luisferh17 <luisferh17@gmail.com>
2018-09-07 20:51:23 -0500

    actualizacion estructura

--- mvc/application/model.php
+++ mvc/application/model.php
@@ -1,6 +1,26 @@
 <?php
 
-class Model {
+class Model extends DataBase {
+
+public function __construct(){
+	parent::__construct('localhost', 'root', '', 'mcds-1747586');
+	parent::connection();
+	//parent::disconection();
+}
+
+public function getAllUsers() {
+		try {
+			$sql = "SELECT * FROM users";
+			$stm = $this->conx->prepare($sql);
+			$stm->execute();
+			return $stm->fetchAll();
+
+		} catch (PDOException $e) {
+			echo $e->getMessage();
+		}
+
+	}
+
  public function getInfo() {
  return array(
  'firstName' 	=> 'Jeremias',
